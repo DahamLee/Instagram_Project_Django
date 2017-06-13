93fcd37fc5464d05fbc07ca5e2aecf996b329621
DahamLee <daham1224@gmail.com>
2017-06-13 16:33:57 +0900

    post_created 구현 post.html comment 반영, 댓글 구현

--- django_apps/post/views.py
+++ django_apps/post/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.template import loader
+from django.urls import reverse
 
 from post.models import Post
+
+User = get_user_model()
 
 
 def index(request):
@@ -19,18 +23,16 @@ def post_list(request):
 
 
 def post_detail(request, post_pk):
-
     # 가져오는 과정에서 예외처리를 한다 (Model.DeosNotExist)
     try:
         post = Post.objects.get(pk=post_pk)
     except Post.DoesNotExist as e:
         # return HttpResponseNotFound('Post not found, detail: {}'.format(e))
-        return redirect('post:post_list')
 
+        # return redirect('post:post_list')
 
-
-
-
+        url = reverse('post:post_list')
+        return HttpResponseRedirect(url)
 
     # render함수는 django.template.loader.get_template함수와
     # django.http.HttpResponse함수를 축약해 놓은 shortcut이다
@@ -47,3 +49,35 @@ def post_detail(request, post_pk):
 
     rendered_string = template.render(context=context, request=request)
     return HttpResponse(rendered_string)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        # get_user_model을 이용해서 얻은 User클래스(Django에서 인증에 사용하는 유저모델)에서 유저 한명을 가져온다
+        user = User.objects.first()
+        # 새 Post객체를 생성하고 DB에 저장
+        post = Post.objects.create(
+            author=user,
+
+            # request.FILES에서 파일 가져오기
+            # 'file'은 POST요청시 input[type="file"]이 가진 name 속성
+            photo=request.FILES['file'],
+
+        )
+        # dict.get 함수
+        comment_string = request.POST.get('comment', '')
+
+        if comment_string:
+            post.comment_set.create(
+                author=user,
+                content = comment_string,
+            )
+
+
+        return redirect('post:post_detail', post_pk=post.pk)
+    else:
+        return render(request, 'post/post_create.html')
+
+
+def post_anyway(request):
+    return redirect('post:post_list')
