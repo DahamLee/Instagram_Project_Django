74a7330ad4dfca0e88055c0fd11c24f4e33b80d8
DahamLee <daham1224@gmail.com>
2017-06-15 12:24:53 +0900

    login view

--- django_apps/member/views.py
+++ django_apps/member/views.py
@@ -1,21 +1,35 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model, login as django_login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
+
+User = get_user_model()
 
 
 def login(request):
     if request.method == 'POST':
-        id = request.POST.get('ID', '')
-        password = request.POST.get('PASSWORD', '')
-        print()
-        user = authenticate(username= id, password = password)
+        # id = request.POST.get('ID', '')
+        # password = request.POST.get('PASSWORD', '')
+        # print('{}'.format(User.objects.get(id=2).username))
+        # print('id = {}, password = {}'.format(id, password))
+        # user = authenticate(username= id, password = password)
+
+        username = request.POST['ID']
+        password = request.POST['PASSWORD']
+        user = authenticate(
+            request,
+            username=username,
+            password=password, )
+
         if user is not None:
+            django_login(request, user)
             return redirect('post:post_list')
         else:
-            return HttpResponse('Login invalid')
+            return HttpResponse('Login credentials invalid')
 
     else:
-        return render(request, 'member/login.html')
+        if request.user.is_authenticated:
+            return redirect('post:post_list')
+        else:
+            return render(request, 'member/login.html')
